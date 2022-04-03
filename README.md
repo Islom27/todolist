2176c10a316ee3c9d7a663fbcd55171034169742
Islom27 <ivokhidov@students.wiut.uz>
2022-04-04 00:58:10 +0500

    some mnor changes

---
+++ README.md
@@ -0,0 +1,30 @@
+# TO DO APPLICATION
+
+There should be description
+
+### Instructions
+1.Download the source code
+
+'''bash
+git clone [link for github]
+'''
+
+or just download the *.zip file
+
+2.Install deps
+
+'''bash
+npm install
+'''
+
+3.Run the app
+'''bash
+node app
+'''
+
+### Deps list
+- express
+- pug
+
+### Link to github repo
+[link for github] 
---
+++ app.js
@@ -0,0 +1,73 @@
+// third party libs
+const express = require('express')
+const app = express()
+
+// node libs
+const fs = require('fs')
+const PORT = 8000
+
+app.set('view engine', 'pug')
+app.use('/static', express.static('puplic')) // assets
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/', (req, res) => {
+    fs.readFile('./data/todos.json', (err, data) => {
+        if (err) throw err 
+
+        const todos = JSON.parse(data)
+
+        res.render('home', { todos: todos })
+    })
+
+    res.render('home')
+})
+
+app.post('/add', (req, res) => {
+    const formData = req.body
+
+    if (formData.todo.trim() = '') {
+        fs.readFile('./data/todos.json', (err, data) => {
+            if (err) throw err
+
+            const todos = JSON.parse(data)
+
+            res.render('home', { error: true, todos: todos })
+        })
+    } else {
+        fs.readFile('./data/todos.json', (err, data) => {
+            if (err) throw err
+
+            const todo = JSON.parse(data)
+
+            const todo = {
+                id: id(),
+                description: formData.todo,
+                done: false
+            }
+
+            todos.push(todo)
+
+            fs.writeFile('./data/todos.json', JSON.stringify(todos), (err) => {
+                if (err) throw err
+
+                fs.readFile('./data/todos.json', (err, data) => {
+                    if (err) throw err
+
+                    const todos = JSON.parse(data)
+
+                    res.render('home', { success: true, todos: todos })
+                })
+            })
+        })
+    }  
+})
+
+app.listen(PORT, (err) => {
+    if (err) throw err
+
+    console.log('This app is running on port ${ PORT }')
+})
+
+function id () {
+    return '_' + Math.random().toString(36).substr(2, 9);
+  }
